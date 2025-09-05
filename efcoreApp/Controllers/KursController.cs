345933add505b6cd007f1d7db01e4092436fb091
Bora Aksoy <121516380+aksoybora@users.noreply.github.com>
2025-09-05 16:53:09 +0300

    Add edit and delete functionality for Kurs
    Implemented Edit and Delete actions in KursController, including corresponding views for editing and deleting Kurs entities. Updated Index view to link to the new actions. This enables users to update and remove Kurs records from the UI.

--- efcoreApp/Controllers/KursController.cs
+++ efcoreApp/Controllers/KursController.cs
@@ -27,7 +27,7 @@ namespace efcoreApp.Controllers
         {
             return View();
         }
-        
+
 
 
         [HttpPost]
@@ -39,5 +39,105 @@ namespace efcoreApp.Controllers
 
             return RedirectToAction("Index", "Kurs");
         }
+
+
+
+        // Güncelleme formu için id vasıtasıyla veritabanında ilgili kursun bulunup form üzerinde gösterilmesi.
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Formu get metoduyla görüntüleyen kişi ile post metoduyla güncelleyen kişinin aynı olup olmadığını token bilgisiyle kontrol eder. (Cross-side attack)
+        public async Task<IActionResult> Edit(int id, Kurs model)
+        {
+            if (id != model.KursId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    if (!_context.Kurslar.Any(o => o.KursId == model.KursId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id);
+
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+
+            _context.Kurslar.Remove(kurs);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
